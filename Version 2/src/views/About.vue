622423ed00fc350f0a2d735d5617f0b25f2a943b
libertus21 <lirbertus@gmail.com>
2019-09-03 21:08:51 -0400

    Version numero 2. En esta, se puso una mejor presentacion

---
+++ Version 2/src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+     <div>
+     <!-- Botonones de "error y Info"-->
+    <v-btn color="error">Falla</v-btn>
+    <v-btn color="info">Informacion</v-btn>
+  </div>
+  </div>
+</template>
---
+++ Version 2/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <login/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import login from '../components/login.vue'
+
+export default Vue.extend({
+  components: {
+   login
+  }
+})
+</script>
